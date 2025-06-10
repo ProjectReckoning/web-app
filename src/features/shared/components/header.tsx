2ae8fae738ced9963deab354b6f1d89a1a349645
martuafernando <fernandosibarani45@gmail.com>
2025-06-10 14:11:05 +0700

    chore: change base project into Next x MUI

---
+++ src/features/shared/components/header.tsx
@@ -0,0 +1,17 @@
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material"
+import MenuIcon from "@mui/icons-material/Menu"
+
+export const Header = () => {
+  return (
+    <AppBar>
+      <Toolbar variant="dense">
+        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" color="inherit" component="div">
+          Photos
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  )
+} 
