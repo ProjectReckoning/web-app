4fa1edac185a1e48efcf1b154ad5a344459f5975
martuafernando <fernandosibarani45@gmail.com>
2025-07-01 14:08:50 +0700

    feat: make default icon for pocket icon outside the contract

--- src/features/pocket/use-cases/get-all-pockets.usecase.ts
+++ src/features/pocket/use-cases/get-all-pockets.usecase.ts
@@ -1,7 +1,18 @@
+import { POCKET_ICON_OPTIONS } from "../constants/pocket-icon-option.constant";
 import { PocketEntity } from "../entities/pocket.entites";
 import pocketRepository from "../repositories/pocket.repository";
 
 export async function getAllPocketsUsecase(): Promise<PocketEntity[]> {
   const result = await pocketRepository.getAllPocket()
-  return result;
+
+  const mappedResult = result.map((it) => {
+    const isIconInsideContract = POCKET_ICON_OPTIONS.includes(it.icon)
+
+    return ({
+      ...it,
+      icon: isIconInsideContract ? it.icon : "pocket"
+    })
+  })
+
+  return mappedResult;
 }
